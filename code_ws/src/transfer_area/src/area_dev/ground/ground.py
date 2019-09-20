8e7aa7f0575cb8ed5ca6ba0f151024ad1f2a9829
ybc82 <charliehw2002@hotmail.com>
2019-09-20 06:28:34 -0700

    Bug fix for ground.py

--- code_ws/src/transfer_area/src/area_dev/ground/ground.py
+++ code_ws/src/transfer_area/src/area_dev/ground/ground.py
@@ -17,7 +17,7 @@ class GroundNode(ServiceNode):
 		ServiceNode.__init__(self)
 		param = AreaParam()
 		self.setRate(0.1)
-		self.Subscriber("scan", LaserScan, self.scan_rx)
+		self.Subscriber("scan", LaserScan, self.scan_rx, queue_size=1)
 		s = rospy.Service('get_ground_status', getGroundStatus, self.check_ground)
 		self.to_scan_ground = False
 		self.b_lidar1_scanned = False
@@ -46,13 +46,11 @@ class GroundNode(ServiceNode):
 			if self.to_scan_ground:
 				# if lidar1 hasn't scanned the wheel, get the data if it's from lidar1
 				if not self.b_lidar1_scanned:
-					print data.header.frame_id, self.frame_id_1
 					if data.header.frame_id == self.frame_id_1:
 						self.scan_1 = copy.deepcopy(data)
 						self.b_lidar1_scanned = True
 				# if lidar2 hasn't scanned the wheel, get the data if it's from lidar2
 				if not self.b_lidar2_scanned:
-					print data.header.frame_id, self.frame_id_2
 					if data.header.frame_id == self.frame_id_2:
 						self.scan_2 = copy.deepcopy(data)
 						self.b_lidar2_scanned = True
@@ -78,38 +76,55 @@ class GroundNode(ServiceNode):
 
 	# @ret: whether ground is clear
 	def process_scans(self):
-		# Lidar 1
+		# Lidar 1, upside-down
 		phi = self.scan_1.angle_min
 		phi_step = self.scan_1.angle_increment
+		print phi, phi_step
 		l_x = self.lidar_pose_1[0]
 		l_y = self.lidar_pose_1[1]
 		l_yaw = self.lidar_pose_1[2]
 		for r in self.scan_1.ranges:
+			if numpy.isinf(r):
+				phi = phi + phi_step
+				continue
+
 			# 1. Transform the point from lidar frame to inertial frame
-			x_l = numpy.cos(phi) * r
-			y_l = numpy.sin(phi) * r
+			x_l = numpy.cos(-phi) * r
+			y_l = numpy.sin(-phi) * r
 			x = x_l * numpy.cos(l_yaw) - y_l * numpy.sin(l_yaw) + l_x
 			y = x_l * numpy.sin(l_yaw) + y_l * numpy.cos(l_yaw) + l_y
 			phi = phi + phi_step
 			# 2. Check if this point is in the ground area
-			if x > self.max_ground_x or x < self.min_ground_x or y > self.max_ground_y or y < self.min_ground_y:
+			if x < self.max_ground_x and x > self.min_ground_x and y < self.max_ground_y and y > self.min_ground_y:
+				print x, self.max_ground_x, self.min_ground_x, y, self.max_ground_y, self.min_ground_y
+				print x<self.max_ground_x, x>self.min_ground_x, y<self.max_ground_y, y>self.min_ground_y
+				print "1- ", x_l, y_l, "; ", x, y, "; ", phi, r
+				print l_x, l_y, l_yaw
 				return False
 
-		# Lidar 2
+		# Lidar 2, upside-down
 		phi = self.scan_2.angle_min
 		phi_step = self.scan_2.angle_increment
 		l_x = self.lidar_pose_2[0]
 		l_y = self.lidar_pose_2[1]
 		l_yaw = self.lidar_pose_2[2]
 		for r in self.scan_2.ranges:
+			if numpy.isinf(r):
+				phi = phi + phi_step
+				continue
+
 			# 1. Transform the point from lidar frame to intertial frame
-			x_l = numpy.cos(phi) * r
-			y_l = numpy.sin(phi) * r
+			x_l = numpy.cos(-phi) * r
+			y_l = numpy.sin(-phi) * r
 			x = x_l * numpy.cos(l_yaw) - y_l * numpy.sin(l_yaw) + l_x
 			y = x_l * numpy.sin(l_yaw) + y_l * numpy.cos(l_yaw) + l_y
 			phi = phi + phi_step
 			# 2. Check if this point is in the ground area
-			if x > self.max_ground_x or x < self.min_ground_x or y > self.max_ground_y or y < self.min_ground_y:
+			if x < self.max_ground_x and x > self.min_ground_x and y < self.max_ground_y and y > self.min_ground_y:
+				print x, self.max_ground_x, self.min_ground_x, y, self.max_ground_y, self.min_ground_y
+				print x<self.max_ground_x, x>self.min_ground_x, y<self.max_ground_y, y>self.min_ground_y
+				print "2- ", x_l, y_l, "; ", x, y, "; ", phi, r
+				print l_x, l_y, l_yaw
 				return False
 
 		# If no objects are detected, return True
