7ea760a5671aef5fbb4b0d41b6fdc16498a20266
jimu-dongyu <dongyu@jimu.ai>
2019-09-20 17:40:47 +0800

    add door state srv

--- code_ws/src/transfer_area/src/area_command/cmd_state.py
+++ code_ws/src/transfer_area/src/area_command/cmd_state.py
@@ -5,6 +5,7 @@ import time
 import rospy
 from command import Command
 from transfer_area.msg import CommandReply, StateCmd
+from transfer_area.srv import getDoorState
 
 class StateCommand(Command):
     def __init__(self):
@@ -19,7 +20,17 @@ class StateCommand(Command):
         self.reply_result()
  
     def reply_result(self):
+        door_state = self.get_doorstate()
         message_text = "area_state: " + str(1)
-        message_text += "\n" + "door_state: " + str([0,0])
+        message_text += "\n" + "door_state: " + str(door_state)
         self.message = str(message_text)
         self.Reply(state = CommandReply.STATE_FINISH)
+
+    def get_doorstate(self):
+        rospy.wait_for_service('door_state')
+        try:
+            state = rospy.ServiceProxy('door_state', getDoorState)
+            res = state()
+            return res.state
+        except rospy.ServiceException, e:
+            return [-1,-1]
