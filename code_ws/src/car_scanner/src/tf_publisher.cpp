26fc8f8e7bdb2d13a90950cb6fc16bf7209f2cf5
ybc82 <charliehw2002@hotmail.com>
2020-09-13 19:46:33 +0900

    Added tf_publisher to publish lidar TFs in a more flexible way.

---
+++ code_ws/src/car_scanner/src/tf_publisher.cpp
@@ -0,0 +1,56 @@
+/*
+ * tf_publisher.cpp
+ *
+ * This is the node to publish (static) tf information.
+ *
+ * Author : yubocheng@jimu.ai
+ *
+ * Copyright (c) 2020 Jimu
+ * All rights reserved.
+ *
+ */
+
+#include <ros/ros.h>
+#include <vector>
+#include <tf/transform_broadcaster.h>
+#include <string>
+
+using namespace std;
+
+#define TF_RATE     1
+#define FRAME_TRANSFER_AREA     "transfer"
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "tf_publisher");
+
+    vector<double>  pose_lidar_1;   // pose [x, y, yaw] of lidar 1
+    vector<double>  pose_lidar_2;
+    string          frame_lidar_1;  // frame ID of lidar 1
+    string          frame_lidar_2;
+    tf::TransformBroadcaster br;
+    tf::Transform transform_lidar_1;
+    tf::Transform transform_lidar_2;
+    ros::Rate node_rate(TF_RATE);
+    tf::Quaternion q;
+    ros::NodeHandle     nh;
+
+    ros::param::get("/lidar_1/pose", pose_lidar_1);
+    ros::param::get("/lidar_2/pose", pose_lidar_2);
+    ros::param::get("/lidar_1/frame_id", frame_lidar_1);
+    ros::param::get("/lidar_2/frame_id", frame_lidar_2);
+
+    transform_lidar_1.setOrigin(tf::Vector3(pose_lidar_1[0], pose_lidar_1[1], 0.0));
+    q.setRPY(0, 0, pose_lidar_1[2]);
+    transform_lidar_1.setRotation(q);    
+    transform_lidar_2.setOrigin(tf::Vector3(pose_lidar_2[0], pose_lidar_2[1], 0.0));
+    q.setRPY(0, 0, pose_lidar_2[2]);
+    transform_lidar_2.setRotation(q);
+
+    while (ros::ok())
+    {
+        br.sendTransform(tf::StampedTransform(transform_lidar_1, ros::Time::now(), FRAME_TRANSFER_AREA, frame_lidar_1));
+        br.sendTransform(tf::StampedTransform(transform_lidar_2, ros::Time::now(), FRAME_TRANSFER_AREA, frame_lidar_2));
+        ros::spinOnce();
+        node_rate.sleep();
+    }
+}
